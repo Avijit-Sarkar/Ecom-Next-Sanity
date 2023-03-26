73461ace8e98a5eebd6ce61982d1525b22f0c268
Avijit <avijitsarkar2222@gmail.com>
2023-03-27 00:45:29 +0530

    Next js Sanity Stripe Basic Ecommerse store

---
+++ components/Product.jsx
@@ -0,0 +1,26 @@
+/* eslint-disable @next/next/no-img-element */
+import { urlFor } from "@/lib/client";
+import Link from "next/link";
+import React from "react";
+
+const Product = ({ product: { image, name, slug, price } }) => {
+  return (
+    <div>
+      <Link href={`/product/${slug.current}`}>
+        <div className="product-card">
+          <img
+            src={urlFor(image && image[0])}
+            width={250}
+            height={250}
+            alt=""
+            className="product-image"
+          />
+          <p className="product-name">{name}</p>
+          <p className="product-price">₹{price}</p>
+        </div>
+      </Link>
+    </div>
+  );
+};
+
+export default Product;
---
+++ sanity-ecomm/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'md7qyt0d',
+    dataset: 'production'
+  }
+})
